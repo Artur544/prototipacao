439b893930f669efc8de209fecd5d635dac60cd2
Artur544 <aocosta06@gmail.com>
2025-09-01 21:37:10 -0300

    teste pwa

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("meu-pwa-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "icone.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
